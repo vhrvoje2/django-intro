b3fbd015b017cd94250039b8dcfa65435ced529c
Hrvoje <vhrvoje2@yahoo.com>
2023-05-17 20:29:16 +0200

    User registration/login

--- base/urls.py
+++ base/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path("register/", views.registerUser, name="register"),
+    path("login/", views.loginUser, name="login"),
+    path("logout/", views.logoutUser, name="logout"),
     path("", views.home, name="home"),
     path("room/<str:pk>", views.room, name="room"),
     path("create-room/", views.create_room, name="create-room"),
